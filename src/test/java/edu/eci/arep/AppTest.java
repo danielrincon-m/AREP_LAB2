c4139dd074f8a3326620ac0257022b8f8ff1ea86
Daniel R <daniel.rincon-m@mail.escuelaing.edu.co>
2021-01-27 21:38:38 -0500

    Pruebas de mean y std deviation implementadas

--- src/test/java/edu/eci/arep/AppTest.java
+++ src/test/java/edu/eci/arep/AppTest.java
@@ -1,20 +1,36 @@
 package edu.eci.arep;
 
+import edu.eci.arep.util.LinkedList;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
+
+    private final Statistics statistics = new Statistics();
+
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void meanTest() throws FileNotFoundException, URISyntaxException {
+        LinkedList<Double> dataset1 = statistics.getDataFromFile("dataset1.txt");
+        LinkedList<Double> dataset2 = statistics.getDataFromFile("dataset2.txt");
+        assertEquals(550.6d, statistics.getMean(dataset1), 0.1d);
+        assertEquals(60.32d, statistics.getMean(dataset2), 0.1d);
+    }
+
+    @Test
+    public void standardDeviationTest() throws FileNotFoundException, URISyntaxException {
+        LinkedList<Double> dataset1 = statistics.getDataFromFile("dataset1.txt");
+        LinkedList<Double> dataset2 = statistics.getDataFromFile("dataset2.txt");
+        assertEquals(572.03d, statistics.getStandardDeviation(dataset1), 0.1d);
+        assertEquals(62.26d, statistics.getStandardDeviation(dataset2), 0.1d);
     }
 }
